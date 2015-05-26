59d5eb22171278098d77b2d27daf8e69f3823c26
Paul Keir <paul.keir@uws.ac.uk>
2015-05-26 21:54:25 +0100

    CMake now checks if the syclone driver script is in the PATH.

--- cmake/FindSYCL.cmake
+++ cmake/FindSYCL.cmake
@@ -1,20 +1,12 @@
 
-find_path(SYCL_DRIVER syclone
-  ENV SYCLONE
-  PATHS tools/driver_script
-)
+find_path(SYCL_DRIVER_SCRIPT syclone)  # Ensure syclone exe script is in PATH
 
-if(SYCL_DRIVER)
-	set(SYCL_FOUND TRUE)
-endif()
+if    (SYCL_DRIVER_SCRIPT)
+  set (SYCL_FOUND TRUE)
+endif (SYCL_DRIVER_SCRIPT)
 
-# Generate appropriate messages
-if(SYCL_FOUND)
-  if(NOT SYCL_FIND_QUIETLY)
-    message("-- Found SYCL driver: ${SYCL_DRIVER}")
-  endif(NOT SYCL_FIND_QUIETLY)
-else(SYCL_FOUND)
-  if(SYCL_FIND_REQUIRED)
-	  message(FATAL_ERROR "-- Could NOT find SYCL (missing: SYCL_DRIVER)")
-  endif(SYCL_FIND_REQUIRED)
-endif(SYCL_FOUND)
+if    (SYCL_FOUND)
+  message ("-- Found SYCL driver: ${SYCL_DRIVER_SCRIPT}/syclone")
+else  (SYCL_FOUND)
+  message ("-- Could NOT find SYCL driver (missing: SYCL_DRIVER_SCRIPT)")
+endif (SYCL_FOUND)
