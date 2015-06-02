1b780a50015e5718074c8976946aada2473611c2
Paul Keir <paul.keir@uws.ac.uk>
2015-06-02 16:56:58 +0100

    Minor cmake update.

--- cmake/FindSYCL.cmake
+++ cmake/FindSYCL.cmake
@@ -1,12 +1,12 @@
 
-find_path(SYCL_CC syclone)  # Ensure syclone exe script is in PATH
+find_path(SYCL_CC_PATH syclone)  # Ensure syclone exe script is in PATH
 
-if    (SYCL_CC)
+if    (SYCL_CC_PATH)
   set (SYCL_FOUND TRUE)
-endif (SYCL_CC)
+endif (SYCL_CC_PATH)
 
 if    (SYCL_FOUND)
-  message ("-- Found SYCL driver: ${SYCL_CC}/syclone")
+  message ("-- Found SYCL driver: ${SYCL_CC_PATH}/syclone")
 else  (SYCL_FOUND)
-  message ("-- Could NOT find SYCL exe driver script (missing: SYCL_CC)")
+  message ("-- Could NOT find SYCL exe driver script (missing: SYCL_CC_PATH)")
 endif (SYCL_FOUND)
