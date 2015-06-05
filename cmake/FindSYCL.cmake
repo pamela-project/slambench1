819db0f2f90f734295aa666b2d616a65e962493e
Paul Keir <paul.keir@uws.ac.uk>
2015-06-05 17:15:37 +0100

    make kfusion-benchmark-sycl works now, but not yet make kfusion-qt-cpp.

--- cmake/FindSYCL.cmake
+++ cmake/FindSYCL.cmake
@@ -1,12 +1,12 @@
 
-find_path(SYCL_CC_PATH syclone)  # Ensure syclone exe script is in PATH
+find_path(SYCL_CC_PATH syclcc)  # Ensure syclcc exe script is in PATH
 
 if    (SYCL_CC_PATH)
   set (SYCL_FOUND TRUE)
 endif (SYCL_CC_PATH)
 
 if    (SYCL_FOUND)
-  message ("-- Found SYCL driver: ${SYCL_CC_PATH}/syclone")
+  message ("-- Found SYCL driver: ${SYCL_CC_PATH}/syclcc")
 else  (SYCL_FOUND)
   message ("-- Could NOT find SYCL exe driver script (missing: SYCL_CC_PATH)")
 endif (SYCL_FOUND)
