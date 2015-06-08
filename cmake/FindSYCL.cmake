21a732f64caef10521565c19c28a3e47dd35e1c1
Paul Keir <paul.keir@uws.ac.uk>
2015-06-08 16:47:13 +0100

    SLAMBench can now compile kfusion-qt-sycl, and even kfusion-qt-cpp, when CXX is set to syclcc. The SYCL code is currently still exactly the same as the cpp version.

--- cmake/FindSYCL.cmake
+++ cmake/FindSYCL.cmake
@@ -1,12 +1,12 @@
 
 find_path(SYCL_CC_PATH syclcc)  # Ensure syclcc exe script is in PATH
 
-if    (SYCL_CC_PATH)
+if    (SYCL_CC_PATH AND "$ENV{CXX}" MATCHES "syclcc")
   set (SYCL_FOUND TRUE)
-endif (SYCL_CC_PATH)
+endif ()
 
 if    (SYCL_FOUND)
   message ("-- Found SYCL driver: ${SYCL_CC_PATH}/syclcc")
 else  (SYCL_FOUND)
-  message ("-- Could NOT find SYCL exe driver script (missing: SYCL_CC_PATH)")
+  message ("-- Could NOT find SYCL driver script (missing: SYCL_CC_PATH)")
 endif (SYCL_FOUND)
